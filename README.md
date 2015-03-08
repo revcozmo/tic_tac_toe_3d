6985fde725e71626b57456e117a1da6093035177
jakoko <aerisgurlie777@gmail.com>
2015-03-07 18:58:50 -0800

    edit README. add gitignore

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-To the instructors: There is an error with my game and firebase.  If more than 2 players can log on, the game becomes unplayable.
-
-
+# 3D Tic Tac Toe
 This Tic Tac Toe game is played in 3D.  Players may connect spaces through the planes of each board.
+
+This was built for a project during my time at web development program.  Please check out the updated version of this game in my repos.
+
+There is an error with the code and firebase.  If more than 2 players log on, the game becomes unplayable.
---
+++ js/GameExtras.js
@@ -0,0 +1,57 @@
+angular
+	.module('ttt3DApp')
+	.factory('GameExtras', GameExtrasFunc)
+
+function GameExtrasFunc() {
+
+	var GameExtras = function(threeDArr, zLength, xLength, yLength) {
+
+		var DDDArr 		= threeDArr;
+		var z 			= zLength;
+		var x 			= xLength;
+		var y 			= yLength;
+
+		this.addBlocks		= addBlocks;
+		this.addBombs		= addBombs;
+		this.activateBomb 	= activateBomb;
+		this.returnDDDArr 	= returnDDDArr;
+
+		function addBlocks(numBlocks) {
+			for(var i = 0; 0 <= numBlocks; i++) {
+				do {
+					var zSpace = Math.floor(Math.random() * z);
+					var xSpace = Math.floor(Math.random() * x);
+					var ySpace = Math.floor(Math.random() * y);
+				}
+				while(DDDArr[zSpace][xSpace][ySpace] !== "");
+				
+				DDDArr[zSpace][xSpace][ySpace] = 'block';
+			}
+		}
+
+		function addBombs(numBombs) {
+			for(var i = 0; 0 <= numBombs; i++) {
+				do {
+					var zSpace = Math.floor(Math.random() * z);
+					var xSpace = Math.floor(Math.random() * x);
+					var ySpace = Math.floor(Math.random() * y);
+				}
+				while(DDDArr[zSpace][xSpace][ySpace] !== "");
+				
+				DDDArr[zSpace][xSpace][ySpace] = 'bomb';
+			}
+		}
+		
+		function activateBomb() {
+			
+		}
+
+		function returnDDDArr() {
+			
+		}
+
+		
+	}
+
+	return GameExtras;
+}
