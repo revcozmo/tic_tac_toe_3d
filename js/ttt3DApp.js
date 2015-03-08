aa2c9ec16c2bee589351a27cd84b217350f23881
jakoko <aerisgurlie777@gmail.com>
2015-03-08 14:06:30 -0700

    set up ui view and templates

--- js/ttt3DApp.js
+++ js/ttt3DApp.js
@@ -1,2 +1,17 @@
 angular
 .module('ttt3DApp', ['firebase', 'ui.router'])
+.config(MainRouter)
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('lounge', {
+            url: '/',
+            templateUrl: "lounge.html"
+        })
+        .state('gamespace', {
+            url: 'gamespace',
+            templateUrl: 'gamespace.html'
+        })
+};
